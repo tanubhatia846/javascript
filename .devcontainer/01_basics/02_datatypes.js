b3ec62ac3325938eee08267c022495e3b2e53bcb
tanubhatia846 <151834014+tanubhatia846@users.noreply.github.com>
2023-12-29 15:14:25 +0000

    container config done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //treat all JS code as newer version
+// alert( 3 + 3 )  //we are using nodejs, not browser
+let name = "tanu"
+let age = 22
+let isloggedin = false
+
+
+
+// null => standalone value
+
+let state //undefined
+let stat = null //null value(empty)
+
+console.log(typeof null) //answer= object
+console.log(typeof undefined) // answer = undefined
